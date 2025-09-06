b39b89753e91e91bdb6bf53efacc71f68ce13556
krydderbarn <97196503+Krydderbarn@users.noreply.github.com>
2025-09-06 11:13:50 +0200

    Added: gameloop with 60fps and creating first sprites

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
 
@@ -15,9 +18,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill("black")
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
